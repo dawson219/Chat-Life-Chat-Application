f5a3c1dfb19f3b58bd6b3d9bdd962fd794c31171
dawsAws <dawson.q@awesomesuite.com>
2023-02-09 10:02:20 +0530

    Removed room name

--- client/src/Components/Join/Join.js
+++ client/src/Components/Join/Join.js
@@ -5,18 +5,16 @@ import './join.css'
 
 export default function Join() {
   const [name , setName] = useState("");
-  const [room , setRoom] = useState("");
+  const [room , setRoom] = useState("general");
 
 
   return (
     <div className="joinOuterContainer">
       <div className="joinInnerContainer">
         <h1 className="heading">Chat-Life</h1>
-        <h1 className="heading">Join a Room</h1>
         <div><input type="text" placeholder='Enter Username...' className='joinInput' onChange={(event)=>setName(event.target.value)} /></div>
-        <div><input type="text" placeholder='Enter Room name...' className='joinInput mt-20' onChange={(event)=>setRoom(event.target.value)} /></div>
         <Link onClick={event => (!name || !room) ? event.preventDefault() : null} to={`/chat?name=${name}&room=${room}`}>
-          <button className="button mt-20" type="submit">Sign In</button>
+          <button className="button mt-20" type="submit">Join</button>
         </Link>
       </div>
     </div>
